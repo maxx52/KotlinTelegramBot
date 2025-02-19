2796e7b0f58c302ff66c67563a197d32bff40489
maxx52 <gospelwork@yandex.ru>
2025-02-19 17:26:52 +0700

    KBT-04 Parse the dictionary file and print the contents

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,31 @@ package ru.maxx52
 import java.io.File
 
 fun main() {
-
     val wordsFile = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.appendText("hello привет\n")
-    wordsFile.appendText("dog собака\n")
-    wordsFile.appendText("cat кошка\n")
 
-    println(wordsFile.readLines())
-    wordsFile.forEachLine { line: String -> println(line) }
+    if (!wordsFile.exists()) {
+        println("Файл не найден: ${wordsFile.absolutePath}")
+        return
+    }
+
+    val dictionary: MutableList<Word> = mutableListOf()
+
+    val lines: List<String> = wordsFile.readLines()
+    for (line in lines) {
+        val parts = line.split("|")
+
+        if (parts.size >= 2) {
+            val original = parts[0].trim()
+            val translate = parts[1].trim()
+            val correctAnswersCount = parts.getOrNull(2)?.trim()?.toInt() ?: 0
+            val word = Word(original, translate, correctAnswersCount)
+            dictionary.add(word)
+        } else {
+            println("Пропуск строки: '$line' (неправильный формат)")
+        }
+    }
+
+    dictionary.forEachIndexed { index, word ->
+        println("${index + 1}. $word")
+    }
 }
