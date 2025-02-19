c8edc3702d1504cdb446b023f0f407525c155267
maxx52 <gospelwork@yandex.ru>
2025-02-20 00:55:16 +0700

    KBT-04 replace toInt() on toIntOrNull()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -19,7 +19,7 @@ fun main() {
         if (parts.size >= 2) {
             val original = parts[0].trim()
             val translate = parts[1].trim()
-            val correctAnswersCount = parts.getOrNull(2)?.trim()?.toInt() ?: 0
+            val correctAnswersCount = parts.getOrNull(2)?.trim()?.toIntOrNull() ?: 0
             val word = Word(original, translate, correctAnswersCount)
             dictionary.add(word)
         } else {
