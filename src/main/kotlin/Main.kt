5588cf5a2f1a33b095891c59a64f72b9b053c6db
maxx52 <gospelwork@yandex.ru>
2025-02-21 00:38:01 +0700

    KTB-06 add statistics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -20,6 +20,8 @@ fun main() {
                 }
                 2 -> {
                     println("Выбран пункт меню \"Статистика\"")
+                    loadDictionary(dictionary)
+                    statistics(dictionary)
                 }
                 0 -> {
                     println("Выход из программы")
@@ -35,14 +37,14 @@ fun main() {
     }
 }
 
-fun loadDictionary(dictionary: MutableList<Word>) {
+fun loadDictionary(dictionary: MutableList<Word>): Int {
     val wordsFile = File("words.txt")
 
     if (!wordsFile.exists()) {
         println("Файл не найден: ${wordsFile.absolutePath}")
-        return
     }
 
+    var correctAnswersCount = 0
     val lines: List<String> = wordsFile.readLines()
     for (line in lines) {
         val parts = line.split("|")
@@ -50,21 +52,29 @@ fun loadDictionary(dictionary: MutableList<Word>) {
         if (parts.size >= 2) {
             val original = parts[0].trim()
             val translate = parts[1].trim()
-            val correctAnswersCount = parts.getOrNull(2)?.trim()?.toIntOrNull() ?: 0
+            correctAnswersCount = parts.getOrNull(2)?.trim()?.toIntOrNull() ?: 0
             val word = Word(original, translate, correctAnswersCount)
             dictionary.add(word)
         } else {
             println("Пропуск строки: '$line' (неправильный формат)")
         }
     }
+    return correctAnswersCount
 }
 
 fun statistics(dictionary: List<Word>) {
+    var totalCount = 0
+    val learnedCount = dictionary.filter { it.correctAnswersCount >= 3 }
+
     if (dictionary.isEmpty()) {
         println("Словарь пуст.")
     } else {
         dictionary.forEachIndexed { index, word ->
+            totalCount++
             println("${index + 1}. $word")
         }
+        val percent = learnedCount.size / totalCount * 100
+
+        println("Выучено ${learnedCount.size} из $totalCount слов | $percent%")
     }
 }
