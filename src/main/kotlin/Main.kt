90b4e42f1d06d5b953ad7a7a933be7b2d986f938
maxx52 <gospelwork@yandex.ru>
2025-03-18 23:22:32 +0700

    KTB-16 Develop buttons and send them to the user in Telegram

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,70 @@
+import java.lang.NumberFormatException
+
+fun getUserInput(prompt: String = "Ваш ответ:"): Int? {
+    println(prompt)
+    return readln().toIntOrNull()
+}
+
+fun Question.asConsoleString(): String {
+    val variants = this.variants
+        .mapIndexed { index: Int, word: Word -> " ${index + 1} - ${word.translate}" }
+        .joinToString(separator = "\n")
+    return this.correctAnswer.questionWord + "\n" + variants + "\n 0 - выйти в меню"
+}
+
+fun main() {
+    val trainer = try {
+        LearnWordsTrainer(3, 4)
+    } catch (e: Exception) {
+        println("Невозможно загрузить словарь")
+        return
+    }
+
+    while (true) {
+        println("Меню:")
+        println("1 - Учить слова")
+        println("2 - Статистика")
+        println("0 - Выход")
+
+        try {
+            when (getUserInput()) {
+                1 -> {
+                    val question = trainer.getNextQuestion()
+                    println("Выбран пункт меню \"Учить слова\"")
+
+                    if (question == null) {
+                        println("Все слова в словаре выучены")
+                        continue
+                    } else {
+                        println("-------------")
+                        println(question.asConsoleString())
+
+                        val userAnswerInput = readln().toIntOrNull()
+                        if (userAnswerInput == 0) continue
+
+                        if (trainer.checkAnswer(userAnswerInput?.minus(1))) {
+                            println("Правильно!\n")
+                            trainer.saveDictionary(question.variants)
+                        } else {
+                            println("Неправильно! ${question.correctAnswer.questionWord} - это ${question.correctAnswer.translate}")
+                        }
+                    }
+                }
+                2 -> {
+                    println("Выбран пункт меню \"Статистика\"")
+                    val statistics = trainer.getStatistic()
+                    println("Выучено ${statistics.learnedCount.size} из ${statistics.totalCount} слов | ${statistics.percent} %")
+                }
+                0 -> {
+                    println("Выход из программы")
+                    return
+                }
+                else -> {
+                    println("Введите число 1, 2 или 0")
+                }
+            }
+        } catch (e: NumberFormatException) {
+            println("Введите число 1, 2 или 0")
+        }
+    }
+}
---
+++ src/main/kotlin/TelegramBotService.kt
@@ -0,0 +1,88 @@
+import java.net.URI
+import java.net.URLEncoder
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.net.http.HttpResponse.BodyHandlers
+
+const val URL_API = "https://api.telegram.org/bot"
+
+class TelegramBotService(
+    private val botToken: String,
+) {
+    private val client: HttpClient = HttpClient.newBuilder().build()
+
+    fun getUpdates(updateId: Int): String? {
+        val urlGetUpdates = "$URL_API$botToken/getUpdates?offset=$updateId"
+        val updateRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+
+        return try {
+            val response = client.send(updateRequest, BodyHandlers.ofString())
+            handleResponse(response)
+        } catch (e: Exception) {
+            println("Error getting updates: ${e.message}")
+            null
+        }
+    }
+
+    fun sendMessage(chatId: Long, text: String): String? {
+        val encodedText = URLEncoder.encode(text, "UTF-8")
+        val urlSendMessage = "$URL_API$botToken/sendMessage?chat_id=$chatId&text=$encodedText"
+        val messageRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
+
+        return try {
+            val response = client.send(messageRequest, BodyHandlers.ofString())
+            handleResponse(response)
+        } catch (e: Exception) {
+            println("Error sending message: ${e.message}")
+            null
+        }
+    }
+
+    fun sendMenu(chatId: Long): String? {
+        val urlSendMessage = "$URL_API$botToken/sendMessage"
+        val sendMenuBody = """
+            {
+                "chat_id": $chatId,
+                "text": "Основное меню",
+                "reply_markup": {
+                    "inline_keyboard": [
+                        [
+                            {
+                                "text": "Изучить слова",
+                                "callback_data": "learn_words_clicked"
+                            },
+                            {
+                                "text": "Статистика",
+                                "callback_data": "statistics_clicked"
+                            }
+                        ]
+                    ]
+                }
+            }
+        """.trimIndent()
+
+        val menuRequest = HttpRequest.newBuilder()
+            .uri(URI.create(urlSendMessage))
+            .header("Content-type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(sendMenuBody))
+            .build()
+
+        return try {
+            val response = client.send(menuRequest, BodyHandlers.ofString())
+            handleResponse(response)
+        } catch (e: Exception) {
+            println("Error sending menu: ${e.message}")
+            null
+        }
+    }
+
+    private fun handleResponse(response: HttpResponse<String>): String? {
+        return if (response.statusCode() == 200) {
+            response.body()
+        } else {
+            println("Error: Received status code ${response.statusCode()}")
+            null
+        }
+    }
+}
